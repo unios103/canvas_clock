c8bde53f07ca3aa62db2e5813a9b8196d78ee188
unios103 <unios103@gmail.com>
2020-01-02 10:46:24 +0900

    個別にcheckを付けれるようにした

--- src/components/Flame.jsx
+++ src/components/Flame.jsx
@@ -7,31 +7,44 @@ export default class Flame extends Component{
     constructor(props){
         super(props);
         this.state={
-            todo:[]
+            todo:[],
+            check:[]
         };
         this.AddTodo=this.AddTodo.bind(this);
         this.RemoveTodo=this.RemoveTodo.bind(this);
+        this.Check=this.Check.bind(this);
     }
 
     AddTodo(e){
         e.preventDefault();
-        const {todo}=this.state;
+        const {todo,check}=this.state;
         todo.push({title:e.target.title.value});
-        this.setState({todo});
+        check.push({status:false});
+        this.setState({todo,check});
+        console.log(this.state);//
+        console.log(this.state.todo,this.state.check);//
         e.target.title.value='';
     }
 
     RemoveTodo(i){
-        const {todo}=this.state;
+        const {todo,check}=this.state;
         todo.splice(i,1);
-        this.setState({todo});
+        check.splice(i,1);
+        this.setState({todo,check});
+    }
+
+    Check(i){
+        const {check}=this.state;
+        check[i].status=!check[i].status;
+        this.setState({check});
+        console.log(check[i].status);//
     }
 
     render(){
         return(
             <div className="flame title">
                 <Form AddTodo={this.AddTodo} />
-                <List todos={this.state.todo} RemoveTodo={this.RemoveTodo} />
+                <List todos={this.state.todo} sta={this.state.check} RemoveTodo={this.RemoveTodo} Check={this.Check} />
             </div>
         )
     }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,22 +2,15 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const List=(props)=>{
-    const [checkbox,change_box]=React.useState(false);
-
-    function Check(e){
-        e.preventDefault();
-        change_box(!checkbox);
-    };
-
-    function Box(checkbox){
-        return checkbox ? <FontAwesomeIcon icon={['far', 'check-square']} /> : <FontAwesomeIcon icon={['far', 'square']} />;
+    function Box(status){
+        return status ? <FontAwesomeIcon icon={['far', 'check-square']} /> : <FontAwesomeIcon icon={['far', 'square']} />;
     }
 
     return(
         <div className="list_box">
             <ul>
                 {props.todos.map((todo,i)=>{
-                    return (<li key={i} className="list"><span className="form_btn checkbox" onClick={Check}>{Box(checkbox)}</span>
+                    return (<li key={i} className="list"><span className="form_btn checkbox" onClick={()=>props.Check(i)}>{Box(props.sta[i].status)}</span>
                     {todo.title}<span className="form_btn trash" onClick={()=>props.RemoveTodo(i)}><FontAwesomeIcon icon={['fas', 'trash']} /></span></li>);
                 })}
             </ul>
