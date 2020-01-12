aa4b6bac3a910dc08a3a475390c008295deaf20e
unios103 <unios103@gmail.com>
2020-01-12 15:59:40 +0900

    clock

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ const { app, BrowserWindow } = require("electron");
 let mainWindow;
 
 function createWindow() {
-    // メインウィンドウを作成します         //minとmaxの設定を有効にする
-    mainWindow = new BrowserWindow({
-        titleBarStyle: "hidden",
-        width: 830,
-        height: 600,
-        /*minWidth:830, maxWidth:830, minHeight:600, maxHeight:600,*/ webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  // メインウィンドウを作成します         //minとmaxの設定を有効にする
+  mainWindow = new BrowserWindow({
+    titleBarStyle: "hidden",
+    width: 420,
+    height: 300,
+    /*minWidth:830, maxWidth:830, minHeight:600, maxHeight:600,*/ webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-    // メインウィンドウに表示するURLを指定します
-    // （今回はmain.jsと同じディレクトリのindex.html）
-    mainWindow.loadFile("./dist/index.html");
+  // メインウィンドウに表示するURLを指定します
+  // （今回はmain.jsと同じディレクトリのindex.html）
+  mainWindow.loadFile("./dist/index.html");
 
-    // デベロッパーツールの起動
-    // mainWindow.webContents.openDevTools();
+  // デベロッパーツールの起動
+  // mainWindow.webContents.openDevTools();
 
-    // メインウィンドウが閉じられたときの処理
-    mainWindow.on("closed", () => {
-        mainWindow = null;
-    });
+  // メインウィンドウが閉じられたときの処理
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
 //  初期化が完了した時の処理
@@ -33,15 +33,15 @@ app.on("ready", createWindow);
 
 // 全てのウィンドウが閉じたときの処理
 app.on("window-all-closed", () => {
-    // macOSのとき以外はアプリケーションを終了させます
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+  // macOSのとき以外はアプリケーションを終了させます
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 // アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
 app.on("activate", () => {
-    // メインウィンドウが消えている場合は再度メインウィンドウを作成する
-    if (mainWindow === null) {
-        createWindow();
-    }
+  // メインウィンドウが消えている場合は再度メインウィンドウを作成する
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,38 @@
 module.exports = {
-    mode: 'development',
-    entry: './src/index.jsx',
-    output: {
-        filename: 'main.js',
-        path: `${__dirname}/dist`
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/react'
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.css/,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            import:true,
-                            modules: false,
-                            sourceMap:true,
-                        }
-                    }
-                ]
+  mode: "development",
+  entry: "./src/clock.jsx",
+  output: {
+    filename: "main.js",
+    path: `${__dirname}/dist`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/react"]
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              import: true,
+              modules: false,
+              sourceMap: true
             }
+          }
         ]
-    },
-    resolve: {
-        extensions: [".js", ".jsx"]
-    }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
 };
